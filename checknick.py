d296b8ff7bb636ed34bd037900fdc8a14b745958
Juniel46 <57242854+Juniel46@users.noreply.github.com>
2022-05-11 12:39:18 +0800

    Add files via upload

---
+++ checknick.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+_banner_ = '''
+   +=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+  +++=== Check Nickname FF ===+++   
+ ++== Tools Check Nickname FF ==++
++= Check Nickname Free Fire Via ID =+
++=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+# author : JC0D3(Juniel Damal)
+# contact: @juniel_damal46
+'''
+id = raw_input('ID Game: ')
+get = requests.get('https://validation.warriors-media.id/trueid/freefire/?id='+id+'&token=JC0D3')
+
+up = get.content
+pu = json.loads(up)
+if "session_key" in up:
+    print
+    print _banner_
+    print 'username:' + pu['identifier']
+    print 'token:' + pu["access_token"]
+    open(user+'-token.txt', 'a').write(pu["access_token"])
+    print
+    print 'saved file to '+user+'-token.txt'
+else:
+    print 'maaf username/password salah'
